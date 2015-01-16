43db12234eeaa489188eb6423ac4c7d447d35cdb
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2015-01-16 20:54:19 +0100

    Correct install_backend git clone command

--- test/e2e/install_backend.sh
+++ test/e2e/install_backend.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # Backend is mandatory for e2e tests, install it from github then start it
-git clone https://$GITHUB_VIGIECHIRO_API_TOKEN:x-oauth-basic@github.com:Scille/vigiechiro-api.git
+git clone https://$GITHUB_VIGIECHIRO_API_TOKEN:x-oauth-basic@github.com/Scille/vigiechiro-api.git
 cd vigiechiro-api
 virtualenv -p /usr/bin/python3 venv
 . ./venv/bin/activate
